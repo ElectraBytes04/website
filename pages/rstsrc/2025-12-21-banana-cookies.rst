1837b314f7ff56b9d061a580c56520ea065565da
ElectraBytes04 <electra-dev.404@protonmail.com>
2026-02-28 15:25:20 -0500

    2026-02-28

---
+++ pages/rstsrc/2025-12-21-banana-cookies.rst
@@ -0,0 +1,48 @@
+======================
+Banana Oatmeal Cookies
+======================
+2025-12-21
+
+Yield: ~36 Cookies
+
+------
+
+Ingredients
+```````````
+
+- 3 Bananas
+- 1/2 Cup Brown Sugar
+- 1/4 Cup Sugar
+- 3/4 Cup (Room Temperature) Butter
+- 1 Large Egg
+- 1 Tablespoon Vanilla
+- (A Small Amount of) Honey
+- 2 Cup Unbleached All Purpose Flour
+- 1/2 Teaspoon Baking Soda
+- 1 Teaspoon Salt
+- 1 Teaspoon Cinnamon
+- 2 1/2 Cup Quick Oats
+- 3/4 Cups Chocolate Chips
+
+
+Notes
+`````
+| I used raw honey since that's all I had.
+| Rolled oats should also work, instead of quick oats.
+
+
+Baking
+``````
+| In your favorite stand mixer:
+|     Cream the butter, sugar, and brown sugar together.
+|     Mix the bananas, vanilla, honey, and egg in.
+| 
+| In a separate bowl:
+|     Whisk the flour, baking soda, salt, oatmeal, cinnamon.
+| 
+| Pour the dry ingredients into the wet ingredients and mix.
+| 
+| Add the chocolate chips.
+| 
+| Scoop and press each ball down slightly.
+| Bake at 357°F for 10 minutes.
---
+++ tool/deploy.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+WPATH="${1:-$PWD/pages}"
+
+RSTSRC="$WPATH/rstsrc"
+
+if [ -z "$EDITOR" ]
+then
+      printf 'WARN: No $EDITOR set. Setting fallback to vi.\n\n'
+      EDITOR=vi
+fi
+
+for rstfile in "$RSTSRC"/*.rst
+do
+      [ -e "$rstfile" ] || continue
+
+      base=`basename "$rstfile" .rst`
+      # htmlfile uses the full path:
+      htmlfile="$WPATH/${base}.html"
+
+      titleref=`sed -n '2p' "$rstfile"`
+      indexref='<li><a href="/pages/'"${base}"'.html">'"${titleref}"'</a></li>'
+      indent="          "
+
+      printf 'Converting file: %s to HTML using Pandoc ...\n' "$rstfile"
+      if ! pandoc --template="$PWD/tool/rst.htmt" --toc -s \
+            "$rstfile" -o "$htmlfile"
+      then
+            printf 'Pandoc failed for file: %s. Skipping ...\n' "$rstfile"
+            continue
+      fi
+
+      # Preview file
+      printf 'Previewing file: %s in a web browser ...\n' "$htmlfile"
+      printf 'You may need to preview the file manually if it fails.\n'
+      python3 -m webbrowser "$htmlfile" >/dev/null 2>&1 &
+
+      printf 'Are the results good? [Y/n] '
+      read -r cgood || cgood=
+
+      case "$cgood" in
+      [Nn]* )
+            printf 'Would you like to open it in a text editor? [Y/n] '
+            read -r copen || copen=
+
+            case "$copen" in
+            [Nn]* )
+                  printf 'Ok.\n\n'
+            ;;
+
+            * )
+                  printf 'Ok. Starting $EDITOR: %s ...\n\n' "$EDITOR"
+                  "$EDITOR" "$htmlfile"
+            ;;
+            esac
+      ;;
+
+      * )
+            printf 'Good! Continuing ...\n\n'
+      ;;
+      esac
+
+      # Adding to index.html
+      printf 'Adding file reference to index.html ...\n\n'
+      if ! grep -qF "${base}.html" index.html
+      then
+            sed '/<h2 id="pages"><ul>/a\'"${indent}${indexref}" index.html \
+                  > index.tmp && mv index.tmp index.html
+      fi
+done
+
+printf "At this point, it is recommended that you pause the script (^Z) and \
+make any changes to files that you want before continuing.
+The next step will commit your changes, and you will be asked to push.\
+\n\nENTER TO CONTINUE\n\n"
+
+read _
+
+printf "Staging changes ...\n"
+git add -A
+
+printf "Committing changes ...\n"
+printf "Commit message is date, with format YYYY-MM-DD\n"
+date=`date +'%Y-%m-%d'`
+git commit -m "$date"
+
+printf "The script will stop here. Push manually if you wish."
